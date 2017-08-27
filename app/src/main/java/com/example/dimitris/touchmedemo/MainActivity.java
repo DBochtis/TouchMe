25b69ad8aded71e496864c332389562d1ae46145
DBochtis <dbochtis@gmail.com>
2017-08-27 12:47:29 +0300

    buttons, functionality

--- app/src/main/java/com/example/dimitris/touchmedemo/MainActivity.java
+++ app/src/main/java/com/example/dimitris/touchmedemo/MainActivity.java
@@ -1,35 +1,64 @@
 package com.example.dimitris.touchmedemo;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private RelativeLayout buttonRel, lvl1Rel, lvl2Rel, lvl3Rel, stopRel;
     private boolean allowed = false;
+    private int counter = 0;
+    private CountDownTimer timer;
+
+    @BindView(R.id.buttonRel)
+    RelativeLayout buttonRel;
+    @BindView(R.id.startRel)
+    RelativeLayout startRel;
+    @BindView(R.id.stopRel)
+    RelativeLayout stopRel;
+    @BindView(R.id.timeTxtView)
+    TextView timeTxtView;
+    @BindView(R.id.scoreTxtView)
+    TextView scoreTxtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        buttonRel = (RelativeLayout) findViewById(R.id.buttonRel);
-        lvl1Rel = (RelativeLayout) findViewById(R.id.lvl1Rel);
-        lvl2Rel = (RelativeLayout) findViewById(R.id.lvl2Rel);
-        lvl3Rel = (RelativeLayout) findViewById(R.id.lvl3Rel);
-        stopRel = (RelativeLayout) findViewById(R.id.stopRel);
+        timeTxtView.setText(getResources().getString(R.string.main_activity_time) + " " + 30 + "\"");
+        scoreTxtView.setText(getResources().getString(R.string.main_activity_score) + " " + 0);
 
-        lvl1Rel.setOnClickListener(this);
-        lvl2Rel.setOnClickListener(this);
-        lvl3Rel.setOnClickListener(this);
+        timer = new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timeTxtView.setText(getResources().getString(R.string.main_activity_time) + " " + millisUntilFinished / 1000 + "\"");
+            }
+
+            public void onFinish() {
+                timeTxtView.setText(getResources().getString(R.string.main_activity_time) + " " + 0);
+                allowed = false;
+            }
+
+        };
+
+        startRel.setOnClickListener(this);
         stopRel.setOnClickListener(this);
-
         buttonRel.setOnClickListener(this);
     }
 
@@ -43,44 +72,88 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 public void run() {
                     Random r = new Random();
                     int tmp = r.nextInt(9);
-                    changeBackgroundColor(tmp);
+                    changeBackgroundDrawable(tmp);
                     changeBackground(time);
                 }
             }, time);
         }
     }
 
-    private void changeBackgroundColor(int position) {
+    private void changeBackgroundDrawable(int position) {
         buttonRel.setBackground(getDrawable(drawablearray[position]));
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.lvl1Rel:
+            case R.id.startRel:
+                counter = 0;
+                timer.start();
                 allowed = true;
                 changeBackground(800);
                 break;
-            case R.id.lvl2Rel:
-                allowed = true;
-                changeBackground(500);
-                break;
-            case R.id.lvl3Rel:
-                allowed = true;
-                changeBackground(300);
-                break;
+//            case R.id.lvl2Rel:
+//                allowed = true;
+//                changeBackground(500);
+//                break;
+//            case R.id.lvl3Rel:
+//                allowed = true;
+//                changeBackground(300);
+//                break;
             case R.id.stopRel:
                 allowed = false;
+                timer.cancel();
                 break;
             case R.id.buttonRel:
-                if (buttonRel.getBackground().getConstantState().equals(getDrawable(R.drawable.green_button).getConstantState())) {
-                    Toast.makeText(getApplicationContext(), "green", Toast.LENGTH_SHORT).show();
-                } else if (buttonRel.getBackground().getConstantState().equals(getDrawable(R.drawable.red_button).getConstantState())) {
-                    Toast.makeText(getApplicationContext(), "red", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "bonus", Toast.LENGTH_SHORT).show();
+                if (allowed) {
+                    Drawable drawable = buttonRel.getBackground();
+                    Drawable green = getDrawable(R.drawable.green_button);
+                    Drawable red = getDrawable(R.drawable.red_button);
+                    Drawable bonus = getDrawable(R.drawable.bonus_button);
+                    if (areDrawablesIdentical(drawable, green)) {
+                        counter++;
+                        scoreTxtView.setText(getResources().getString(R.string.main_activity_score) + " " + counter);
+                    } else if (areDrawablesIdentical(drawable, red)) {
+                        counter--;
+                        scoreTxtView.setText(getResources().getString(R.string.main_activity_score) + " " + counter);
+                    } else if (areDrawablesIdentical(drawable, bonus)){
+                        counter = counter + 5;
+                        scoreTxtView.setText(getResources().getString(R.string.main_activity_score) + " " + counter);
+                    }
                 }
                 break;
         }
+    }
+
+    public static boolean areDrawablesIdentical(Drawable drawableA, Drawable drawableB) {
+        Drawable.ConstantState stateA = drawableA.getConstantState();
+        Drawable.ConstantState stateB = drawableB.getConstantState();
+        // If the constant state is identical, they are using the same drawable resource.
+        // However, the opposite is not necessarily true.
+        return (stateA != null && stateB != null && stateA.equals(stateB))
+                || getBitmap(drawableA).sameAs(getBitmap(drawableB));
+    }
+
+    public static Bitmap getBitmap(Drawable drawable) {
+        Bitmap result;
+        if (drawable instanceof BitmapDrawable) {
+            result = ((BitmapDrawable) drawable).getBitmap();
+        } else {
+            int width = drawable.getIntrinsicWidth();
+            int height = drawable.getIntrinsicHeight();
+            // Some drawables have no intrinsic width - e.g. solid colours.
+            if (width <= 0) {
+                width = 1;
+            }
+            if (height <= 0) {
+                height = 1;
+            }
+
+            result = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(result);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+        }
+        return result;
     }
 }
